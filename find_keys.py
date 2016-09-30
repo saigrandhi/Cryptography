9795972f160eed533362ec85a5acaf62368d4299
Sai Grandhi <sgrndh@gmail.com>
2016-09-30 10:06:14 -0400

    :sparkles: Added DES Algorithm and is_prime

---
+++ find_keys.py
@@ -0,0 +1,14 @@
+# For RSA
+
+'''
+Check if given number is prime.
+'''
+def is_prime(num):
+    if num == 2:
+        return True
+    if num < 2 or num % 2 == 0:
+        return False
+    for n in xrange(3, int(num**0.5)+2, 2):
+        if num % n == 0:
+            return False
+    return True
